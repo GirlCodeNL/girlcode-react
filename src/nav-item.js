a5ae4d6e48dbc41d5750dfd80b5127b300cc64cf
Katja Hollaar <afkatja@gmail.com>
2017-03-24 16:05:57 +0100

    Dropdown nav - added behavior and styling

--- src/nav-item.js
+++ src/nav-item.js
@@ -2,6 +2,18 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
 export default class NavItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subnavOpen: false,
+    };
+    let subnavOpen = false;
+    this.toggle = (e) => {
+      e.preventDefault();
+      subnavOpen = !subnavOpen;
+      this.setState({subnavOpen: subnavOpen});
+    }
+  }
   static propTypes = {
     title: PropTypes.string,
     url: PropTypes.string,
@@ -11,9 +23,24 @@ export default class NavItem extends Component {
   static defaultProps = { title: 'item1', url: 'nu.nl' };
 
   render() {
+    let subnav = '';
+    if (this.props.subnav) {
+      const className = this.state.subnavOpen ? 'expanded' : '';
+      subnav = <ul className={`subnav ${className}`}>
+        {this.props.subnav.map((item, i) =>
+          <li key={`subnav-item-${i}`}>
+            <Link className="waves-effect waves-light" to={item.url}>{item.title}</Link>
+          </li>
+        )}
+      </ul>;
+    }
+
     return (
       <li>
-        <Link className="waves-effect waves-light" to={this.props.url} activeClassName="active">{this.props.title}</Link>
+        <Link className={`waves-effect waves-light ${this.props.subnav ? 'has-subnav dropdown-button' : ''}`} to={this.props.url} activeClassName="active" onClick={this.toggle}>
+          {this.props.title}
+        </Link>
+        {subnav}
       </li>
     );
   }
