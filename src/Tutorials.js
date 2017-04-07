ce08df1b7c866ceb757e1305476c9f76a35a6d77
Katja Hollaar <afkatja@gmail.com>
2017-04-07 15:33:54 +0200

    Tutorials - adjusted path names

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -9,8 +9,8 @@ export default class Tutorials extends Component {
           <h2>Tutorials</h2>
           <p>Here you can find the tutorials for the workshops we host during our meetups, including slides and demos.</p>
           <ul>
-            <li><Link to="/tutorials/tictactoe-tutorial">Tictactoe game with meteor.js</Link></li>
-            <li><Link to="/tutorials/vision-api-tutorial">Vision API basics</Link></li>
+            <li><Link to="/tutorials/tictactoe">Tictactoe game with meteor.js</Link></li>
+            <li><Link to="/tutorials/vision-api">Vision API basics</Link></li>
           </ul>
         </section>
         {this.props.children}
