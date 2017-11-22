425f298618a1a326236df92b4f279d9b6b6f2fe8
Katja Hollaar <afkatja@gmail.com>
2017-11-22 12:52:17 +0100

    Slides - added page

---
+++ src/Slides.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Slides() {
+  return (
+    <div className="page-container">
+      <section>
+        <h2>Slides</h2>
+
+      </section>
+    </div>
+  );
+}
