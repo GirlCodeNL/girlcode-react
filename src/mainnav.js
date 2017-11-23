c8df02b40765ea5e1e10ef2cba4ca6617e44332f
Katja Hollaar <afkatja@gmail.com>
2017-11-23 12:56:49 +0100

    Main nav - implementing mobile icon and nav

--- src/mainnav.js
+++ src/mainnav.js
@@ -6,15 +6,52 @@ import classNames from 'classnames';
 import NavItem from './nav-item';
 import logo from './images/logo-girlcode.png';
 
+const items = [
+  {
+    url: '/',
+    title: 'Home',
+    id: 'home',
+  },
+  {
+    url: '/about',
+    title: 'About',
+    id: 'about',
+  },
+  {
+    url: '/events',
+    title: 'Events',
+    id: 'events',
+  },
+  {
+    url: '/tutorials',
+    title: 'Tutorials',
+    id: 'tutorials',
+    subnav: [
+      { title: 'Tictactoe', url: '/tutorials/tictactoe', id: 'tictactoe' },
+      { title: 'Vision API', url: '/tutorials/vision-api', id: 'vision-api' },
+    ],
+  },
+  {
+    url: '/slides',
+    title: 'Slides',
+    id: 'slides',
+  },
+  {
+    url: '/blog',
+    title: 'Blog',
+    id: 'blog',
+  },
+];
+
 export default class MainNav extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sticky: false,
+      mobileNavOpen: false,
     };
     this.onScroll = () => {
       const $mainNav = document.querySelector('.js-nav');
-      if (!$mainNav) return;
 
       const topHeight = $mainNav.offsetHeight;
       if ($mainNav.classList.contains('mobile-nav-open')) {
@@ -23,52 +60,29 @@ export default class MainNav extends Component {
       const scrollPos = document.documentElement.scrollTop || window.scrollY;
       this.setState({ sticky: scrollPos > topHeight });
     };
+    this.onMobileButtonClick = () => {
+      this.setState({ mobileNavOpen: !this.state.mobileNavOpen });
+    };
   }
 
   componentDidMount() {
     document.addEventListener('scroll', () => this.onScroll());
+    document.body.querySelector('main').addEventListener('click', () => {
+      this.setState({ mobileNavOpen: !this.state.mobileNavOpen });
+    });
   }
   render() {
-    const items = [
-      {
-        url: '/',
-        title: 'Home',
-        id: 'home',
-      },
-      {
-        url: '/about',
-        title: 'About',
-        id: 'about',
-      },
-      {
-        url: '/events',
-        title: 'Events',
-        id: 'events',
-      },
-      {
-        url: '/tutorials',
-        title: 'Tutorials',
-        id: 'tutorials',
-        subnav: [
-          { title: 'Tictactoe', url: '/tutorials/tictactoe', id: 'tictactoe' },
-          { title: 'Vision API', url: '/tutorials/vision-api', id: 'vision-api' },
-        ],
-      },
-      {
-        url: '/slides',
-        title: 'Slides',
-        id: 'slides',
-      },
-      {
-        url: '/blog',
-        title: 'Blog',
-        id: 'blog',
-      },
-    ];
     return (
-      <nav className={classNames('site-nav nav-wrapper js-nav', { 'sticky-nav': this.state.sticky })}>
+      <nav className={classNames('site-nav nav-wrapper js-nav', { 'sticky-nav': this.state.sticky, 'mobile-nav-open': this.state.mobileNavOpen })}>
         <Link to="/" className="logo-link"><img src={logo} alt="GirlCode" className="logo" /></Link>
-        <ul>
+        <button
+          type="button"
+          className={classNames('mobile-icon js-mobile-button', { 'mobile-icon-open': this.state.mobileNavOpen })}
+          onClick={() => console.log(this.state.mobileNavOpen) || this.onMobileButtonClick()}
+        >
+          <i />
+        </button>
+        <ul className="nav-primary">
           { items.map(item => (
             <NavItem key={`identifier${item.id}`} title={item.title} url={item.url} subnav={item.subnav} />
           ))}
